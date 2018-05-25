fa506a8035f18b8ba6ee4da2215ab83e42ab098a
deepankar01 <deepankar01agrawal@gmail.com>
2018-05-25 18:21:57 +0530

    added data for the assignment

--- nodeJS/002-Basic-HTTP-Routes/index.js
+++ nodeJS/002-Basic-HTTP-Routes/index.js
@@ -1,15 +1,16 @@
 var http = require('http');
 var url = require('url');
+var data = require('./data.json');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {
-    'Content-type': 'text/plan'
+    'Content-type': 'text/plain'
   });
   response.write('Hello Node JS Server Response');
   response.end();
 
   const {pathname,query} = url.parse(request.url);
-
+  console.log(data);
   console.log("pathname", pathname);
   console.log("queryParameters", query);
 
